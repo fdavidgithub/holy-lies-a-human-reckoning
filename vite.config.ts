f13821303d1cd8450951e4cc57e651d8156b5827
francis DAVID <contato@fdavid.com.br>
2025-12-15 21:48:24 -0300

    fix: portable local git and loveble

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,26 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/holy-lies-a-human-reckoning/',
+  base:
+    mode === "github"
+      ? "/holy-lies-a-human-reckoning/"
+      : "/",
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
